b0991e2b46679327bf5b66781514729603d92d9d
firdausnanda <firdausnanda46@gmail.com>
2026-08-27 20:54:17 +0700

    [FIX] Verifikasi TTE

--- resources/views/verifikasi/dokumen-angkutan.blade.php
+++ resources/views/verifikasi/dokumen-angkutan.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verifikasi Dokumen Angkutan</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/js/app.jsx'])
 </head>
 <body class="bg-gray-100 min-h-screen flex items-center justify-center p-4">
     <div class="bg-white rounded-xl shadow-lg max-w-md w-full overflow-hidden">
