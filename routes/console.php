bd047a52981aa40b915b635d6f2918d176f96823
firdausnanda <firdausnanda46@gmail.com>
2026-09-01 13:45:24 +0700

    [FIX] Schedule Database

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Menjadwalkan backup database setiap jam 2 pagi
+Schedule::command('backup:run --only-db')->dailyAt('02:00');
